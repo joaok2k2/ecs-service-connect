fa4ce5b61b24d3b72ae85ab099a765286cee6223
joaok2k2 <joak2k2@gmail.com>
2023-08-07 01:08:58 -0300

    app - index from storage-files

---
+++ storage-files/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
+
+const port = 3500;
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/");
+        
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname + Date.now() + path.extname(file.originalname));
+    }
+})
+const upload = multer({storage})
+
+app.get("/", (req, res) => {
+    try {
+        res.sendStatus(200)
+    } catch (error){
+        res.sendStatus(500)
+    }
+})
+
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(200).send("Arquivo Recebido!");
+})
+
+app.listen(port, () => {
+    console.log("Servidor rodando!");
+})
